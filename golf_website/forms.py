e475fd437c4af64014e2f0157c95c2bb0cfa9975
dorstrauss <dorshtrauss11@gmail.com>
2023-10-17 19:50:46 +0300

    added fields to the signup form

--- golf_website/forms.py
+++ golf_website/forms.py
@@ -1,12 +1,20 @@
 from django.contrib.auth.forms import UserCreationForm, UserChangeForm
+from django import forms
 from golf_website.models import GolfUser
 
 
 class GolfUserCreationForm(UserCreationForm):
 
+    first_name = forms.CharField(required=True)
+    last_name = forms.CharField(required=True)
+    email = forms.EmailField(required=True)
+    height = forms.IntegerField(required=False)
+    handicap = forms.IntegerField(required=False)
+    player_image = forms.ImageField(required=False)
+
     class Meta:
         model = GolfUser
-        fields = ('first_name', 'last_name', 'email', 'sensor_id')
+        fields = ('first_name', 'last_name', 'email', 'sensor_id', 'height', 'handicap', 'player_image')
 
 
 
--- golf_website/models.py
+++ golf_website/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 from django.core.validators import MaxValueValidator, MinValueValidator
+from django.dispatch import receiver
+from django.db.models.signals import pre_save
 
 
 class GolfUser(AbstractUser):
@@ -12,3 +14,8 @@ class GolfUser(AbstractUser):
 
     def __str__(self):
         return '{first_name} {last_name}'.format(first_name=self.first_name, last_name=self.last_name)
+
+
+@receiver(pre_save, sender=GolfUser)
+def fill_username(sender, instance, **kwargs):
+    instance.username = instance.first_name + ' ' + instance.last_name
