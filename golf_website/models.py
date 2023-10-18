5e25a3452ffa9dc800047ba488caee882e71e749
dorstrauss <dorshtrauss11@gmail.com>
2023-10-18 19:05:31 +0300

    changed login to email and password

--- golf_website/models.py
+++ golf_website/models.py
@@ -13,10 +13,13 @@ class GolfUser(AbstractUser):
     handicap = models.IntegerField(null=True, validators=[MinValueValidator(0), MaxValueValidator(36)])
     sensor_id = models.CharField(unique=True, max_length=25, default="")
 
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
     def __str__(self):
         return '{first_name} {last_name}'.format(first_name=self.first_name, last_name=self.last_name)
 
 
 @receiver(pre_save, sender=GolfUser)
 def fill_username(sender, instance, **kwargs):
-    instance.username = instance.first_name + ' ' + instance.last_name
+    instance.username = instance.first_name + '_' + instance.last_name
